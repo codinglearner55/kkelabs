d6249f2a43134325814b176e21b5b499257eb784
codinglearner55 <139243562+codinglearner55@users.noreply.github.com>
2024-12-19 22:13:17 +0000

    20241219-1012: comments added to tmux install script

--- setup_tmux.bash
+++ setup_tmux.bash
@@ -1,6 +1,8 @@
+## Useful commands after a lab has started up
 yum list tmux
 sudo yum -y install tmux 
 tmux new-session -d
 tmux list-sessions > tmuxsessiondetails.txt
 tmux set-option -g history-limit 10000
+echo
 echo "tmux is installed, running with buffer size increased"
