3a82bda1e80580fccf7237454f67b3bf4168aebe
codinglearner55 <thor@jumphost.stratos.xfusioncorp.com>
2024-12-19 16:57:01 +0000

    update iptables script

--- install_iptables.bash
+++ install_iptables.bash
@@ -5,5 +5,8 @@ hostarr=(stapp01 stapp02 stapp03)
 for i in ${!userarr[*]}
   do 
     echo "starting ${userarr[i]}@${hostarr[i]}"
-    ssh ${userarr[i]}@${hostarr[i]} sudo yum -y install iptables
+    #ssh ${userarr[i]}@${hostarr[i]} sudo yum -y install iptables
+    #ssh ${userarr[i]}@${hostarr[i]} sudo yum -y install iptables-services
+    ssh ${userarr[i]}@${hostarr[i]} sudo systemctl enable iptables
+    ssh ${userarr[i]}@${hostarr[i]} sudo systemctl start iptables
   done
